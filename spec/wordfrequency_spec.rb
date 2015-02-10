5ef7bcf268b61d4f9c68e5da71a0529f11e9bf5b
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-10 17:47:14 +0000

    wordfrequency - removes twitter handles

--- spec/wordfrequency_spec.rb
+++ spec/wordfrequency_spec.rb
@@ -4,6 +4,8 @@ describe 'WordFrequency' do
 
   let(:freq){WordFrequency.new}
 
+  test_array = {"how"=> 4, "devolution"=> 4, "see"=> 4, "trade"=> 6, "growth"=> 6, "EU"=> 6, "sector"=> 6, "key"=> 6, "referendum"=>6, "pay"=>7, "britishchambers"=> 8, "bccconf"=>8, "business"=>8, "Cameron"=> 10, "DavidCameron"=> 11, "BCCConf"=> 41}
+
   it 'should read a file and return an string' do
     expect(freq.read_file('data.txt')).to be_an_instance_of(String)
   end
@@ -21,8 +23,14 @@ describe 'WordFrequency' do
   end
 
   it 'should return the top n number of words' do
-    test_array = {"how"=> 4, "devolution"=> 4, "see"=> 4, "trade"=> 6, "growth"=> 6, "EU"=> 6, "sector"=> 6, "key"=> 6, "referendum"=>6, "pay"=>7, "britishchambers"=> 8, "bccconf"=>8, "business"=>8, "Cameron"=> 10, "DavidCameron"=> 11, "BCCConf"=> 41}
     expect(freq.filter_top_words(5, test_array)).to eq([["BCCConf", 41], ["DavidCameron", 11],  ["Cameron", 10], ["business", 8], ["bccconf", 8]])
+  end
+
+  it 'should not return any twitter handles' do
+    text = freq.read_file('data.txt')
+    array = freq.make_array(text)
+    expect(array).not_to include(['DavidCameron', 11])
+    puts array
   end
 
 
