61a25b13692efcc24a57b12cc1e5d7321cad4da3
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-11 10:18:02 +0000

    links - all tests passing, can find top recurring links

--- lib/links.rb
+++ lib/links.rb
@@ -4,15 +4,15 @@ class Links
 
   include Helper
 
-  # def find_top_results(number, file_path)
-  #   array = read_file(file_path)
-  #   frequnecy_hash = count_freq(array)
-  #   filter_top_results(number, frequnecy_hash)
-  # end
+  def find_top_results(number, file_path)
+    array = read_file(file_path)
+    frequnecy_hash = count_freq(array)
+    filter_top_results(number, frequnecy_hash)
+  end
 
   def read_file(file_path)
     text = File.read(file_path)
-    text.scan(/http\w+/).flatten
+    text.scan(/https?:\/\/[A-Za-z0-9._\/&?#]+/).flatten
   end
 
 end
