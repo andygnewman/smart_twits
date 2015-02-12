9926b222bf41bc4ebe3c5bb23abc02c22d535edc
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-12 18:07:08 +0000

    passing 4 tests

--- spec/tweets_spec.rb
+++ spec/tweets_spec.rb
@@ -22,6 +22,10 @@ describe 'Tweets' do
       expect(tweets.read_file('spec/test_data/test.txt')).to be_an_instance_of(Array)
     end
 
+    it 'can take an array of sentences and symbols and return a list of words' do
+      expect(tweets.extract_words(["hello &*", "HOW are ! you"])).to eq(["hello", "", "how", "are", "", "you"])
+    end
+
   end
 
 end
