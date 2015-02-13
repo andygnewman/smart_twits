239d436144974b5b62b66c26f52bbb61174275bf
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-13 08:08:03 +0000

    refactoring code into helper module

--- lib/helper.rb
+++ lib/helper.rb
@@ -10,6 +10,24 @@ module Helper
     array
   end
 
+  def read_link_file(file_path)
+    text = File.read(file_path)
+    text.scan(/https?:\/\/[A-Za-z0-9._\/&?#]+/).flatten
+  end
+
+  def parse_file(file_path, regex)
+    text = File.read(file_path)
+    text.scan(/@\w+/).flatten
+  end
+
+  def extract_words(array)
+    array.join(" ").split(" ").map{ |w| w.downcase.gsub(/\B[@#]\S+\b|(?:f|ht)tps?:\/[^\s]+|[^a-zA-Z0-9%\s]/, "")}
+  end
+
+  def reject_words(array)
+    array.reject {|w| REJECT_WORDS.include?(w) }
+  end
+
   def count_freq(array)
     word_freq = Hash.new(0)
     array.each {|word| word_freq[word]+=1}
@@ -20,4 +38,12 @@ module Helper
     hash.sort_by {|k, v| v}.reverse[0..(number-1)]
   end
 
+  def find_top_words(number, array)
+    words = extract_words(array)
+    useful_words = reject_words(words)
+    frequency_hash = count_freq(useful_words)
+    filter_top_results(number, frequency_hash)
+  end
 end
+
+
--- spec/tweets_spec.rb
+++ spec/tweets_spec.rb
@@ -35,7 +35,15 @@ describe 'Tweets' do
     end
 
     it 'can return the n most frequently occuring words' do
-      expect(tweets.find_words(3, 'spec/test_data/test.txt')).to eq([["your", 14], ["amp", 13], ["valentines", 12]])
+      expect(tweets.find_words(3, 'spec/test_data/test.txt')).to eq([["amp", 13], ["day", 12], ["valentines", 12]])
+    end
+
+  end
+
+  context 'find retweets' do
+
+    xit 'can read a tweets file and return an array of tweets and the number of retweets' do
+      expect(tweets.find_retweets(3, 'spec/test_data/test.txt')).to include(["RT @CNJohansens: What's better than roses this #ValentinesDay? A Rose Petal Body Polish treatment at Armani Hotel Dubai! http://t.co/eD4wIR…", 2])
     end
 
   end
