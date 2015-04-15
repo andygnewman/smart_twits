bc8e6037e8c5b0a1629eb842cbf75545fd44f458
Emily Sas <emily.bronwen.sas@gmail.com>
2015-04-15 10:53:13 +0100

    passing first test for word_finder

--- lib/word_finder.rb
+++ lib/word_finder.rb
@@ -4,4 +4,10 @@ class WordFinder
 
   include Helper
 
+  def find_words(number, file_path)
+    words_array = read_file(file_path)
+    words = words_array.map{|el| el[:text]}
+    find_top_words(number, words)
+  end
+
 end
