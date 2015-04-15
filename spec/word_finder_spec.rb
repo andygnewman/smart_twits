5460c4c1abc126a388466109e12a2e0dbac8ef1f
Emily Sas <emily.bronwen.sas@gmail.com>
2015-04-15 10:53:41 +0100

    passing all tests for word_finder

--- spec/word_finder_spec.rb
+++ spec/word_finder_spec.rb
@@ -11,19 +11,19 @@ describe 'WordFinder' do
       expect(words.read_file('spec/test_data/test.txt')).to be_an_instance_of(Array)
     end
 
-    xit 'can take an array of sentences and symbols and return a list of words' do
+    it 'can take an array of sentences and symbols and return a list of words' do
       expect(words.extract_words(["hello &*", "HOW are ! you"])).to eq(["hello", "", "how", "are", "", "you"])
     end
 
-    xit 'can take an array of words and reject unwanted words' do
+    it 'can take an array of words and reject unwanted words' do
       expect(words.reject_words(["and", "it", "", "was", "stupendous"])).to eq(["stupendous"])
     end
 
-    xit 'can count the frequency with which the words appear' do
+    it 'can count the frequency with which the words appear' do
       expect(words.count_freq(["hello", "how", "hello", "you", "how"])).to eq({"hello"=>2, "how"=>2, "you"=>1})
     end
 
-    xit 'can return the n most frequently occuring words' do
+    it 'can return the n most frequently occuring words' do
       expect(words.find_words(3, 'spec/test_data/test.txt')).to eq([["special", 2], ["know", 2], ["rose", 2]])
     end
 
