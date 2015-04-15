5ff8199bfa3580915f7ccf52bc4071b8fef74817
Emily Sas <emily.bronwen.sas@gmail.com>
2015-04-15 11:01:13 +0100

    passing test for follower_finder

--- lib/follower_finder.rb
+++ lib/follower_finder.rb
@@ -3,6 +3,12 @@ require_relative 'helper'
 class FollowerFinder
 
   include Helper
-
+  
+  def find_followers(file_path)
+    followers_array = read_file(file_path)
+    array = []
+    followers_array.map{|el| array << [el[:name], el[:text], el[:followers]]}
+    array
+  end
 
 end
